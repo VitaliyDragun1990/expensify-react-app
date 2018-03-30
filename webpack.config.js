65e76bb8f2d19c0feb56a4449090a004e265ce83
Vitaly Dragun <vdrag00n90@gmail.com>
2018-03-30 16:40:00 +0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,58 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-// entry point -> output
-module.exports = {
-    // entry point of our app
-    entry: './src/app.js',
-    // output file containing all our js code
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        // different rules for webpack
-        rules: [
-            // rule for using babel to compile js files
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            // rule for serving css and scss files
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    // source map to efficiently track errors in devTools browser console
-    devtool: 'cheap-module-eval-source-map',
-    // development server specific to webpack
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+      // entry point of our app
+      entry: './src/app.js',
+      // output file containing all our js code
+      output: {
+          path: path.join(__dirname, 'public'),
+          filename: 'bundle.js'
+      },
+      module: {
+          // different rules for webpack
+          rules: [
+              // rule for using babel to compile js files
+              {
+                  loader: 'babel-loader',
+                  test: /\.js$/,
+                  exclude: /node_modules/
+              },
+              // rule for serving css and scss files
+              {
+                  test: /\.s?css$/,
+                  use: CSSExtract.extract({
+                      use: [
+                          {
+                              loader: 'css-loader',
+                              options: {
+                                  sourceMap: true
+                              }
+                          },
+                          {
+                              loader: 'sass-loader',
+                              options: {
+                                  sourceMap: true
+                              }
+                          }
+                      ]
+                  })
+              }
+          ]
+      },
+      plugins: [
+          CSSExtract
+      ],
+      // source map to efficiently track errors in devTools browser console
+      devtool: isProduction ? 'source-map' : 'inline-source-map',
+      // development server specific to webpack
+      devServer: {
+          contentBase: path.join(__dirname, 'public'),
+          historyApiFallback: true
+      }
+  }
 };
